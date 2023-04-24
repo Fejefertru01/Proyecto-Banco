5f3ea7b091a6d3ad68275ec5a7b4678ba456e516
unknown <ppi0002@alu.medac.es>
2023-04-24 11:22:59 +0200

    Añadimos constructor de Clientes

--- prBanco/src/prBanco/Clientes.java
+++ prBanco/src/prBanco/Clientes.java
@@ -1,5 +1,11 @@
 package prBanco;
 
 public class Clientes {
+	private String nombre;
+	private String apellidos;
 
+	public Clientes(String nombre, String apellidos) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
 }
