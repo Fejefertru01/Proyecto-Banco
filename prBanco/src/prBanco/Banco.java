06a286ca8eec4996a9a2bfa933aaccc557c2eafd
unknown <fejefertru@gmail.com>
2023-04-24 11:08:13 +0200

    Añadido constructor del banco

--- prBanco/src/prBanco/Banco.java
+++ prBanco/src/prBanco/Banco.java
@@ -2,4 +2,16 @@ package prBanco;
 
 public class Banco {
 
+	private int iban;
+	private double saldo;
+
+	public Banco() {
+		iban = 0;
+		saldo = 0;
+	}
+
+	public Banco(int iban, double saldo) {
+		this.iban = iban;
+		this.saldo = saldo;
+	}
 }
