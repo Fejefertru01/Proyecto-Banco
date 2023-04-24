2e4bd67bec488e9ca8e85b415ef72f484888732b
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2023-04-24 11:11:27 +0200

    Añadido setters a banco

--- prBanco/src/prBanco/Banco.java
+++ prBanco/src/prBanco/Banco.java
@@ -14,4 +14,12 @@ public class Banco {
 		this.iban = iban;
 		this.saldo = saldo;
 	}
+
+	public void setIban(int iban) {
+		this.iban = iban;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 }
