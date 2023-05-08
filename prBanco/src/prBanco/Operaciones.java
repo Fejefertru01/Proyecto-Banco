8322d4ccf811d5a5e325de7833a2bcb58cd619f4
unknown <fejefertru@gmail.com>
2023-05-08 10:51:49 +0200

    Metodo prestamo añadido

--- prBanco/src/prBanco/Operaciones.java
+++ prBanco/src/prBanco/Operaciones.java
@@ -1,5 +1,12 @@
 package prBanco;
 
-public class Operaciones {
-
+public abstract class Operaciones {
+/**
+ * Metodo para sumar dinero a una cuenta
+ * @param c cuenta a la que se le va a añadir el dinero.
+ * @param cantidad cantidad de dinero a sumar
+ */
+	public void prestamo(Cuenta c, int cantidad) {
+		 c.setSaldo(c.getsaldo()+cantidad);
+	}
 }
