c24fe17cdc89a3ddf30815528ef535412ddb3b64
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2023-05-08 10:33:35 +0200

    Añadido comentarios de la clase cliente, ademas de arreglar los metodos getters

--- prBanco/src/prBanco/Clientes.java
+++ prBanco/src/prBanco/Clientes.java
@@ -1,32 +1,69 @@
 package prBanco;
 
+/**
+ * 
+ * @author Pablo Jesús Calvente Ramírez, Fernando Jesús Fernández Trujillo,
+ *         Pablo Perez Iza
+ * @version 1.1
+ */
 public class Clientes {
 	private String nombre;
 	private String apellidos;
 
+	/**
+	 * Contructor de la clase cliente.
+	 * 
+	 * @param nombre    nombre del cliente
+	 * @param apellidos apellidos del cliente
+	 */
 	public Clientes(String nombre, String apellidos) {
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 	}
 
+	/**
+	 * Cambia el nombre del cliente
+	 * 
+	 * @param nombre nuevo nombre del cliente
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Cambia el apellido del cliente
+	 * 
+	 * @param apellidos nuevo apellido del cliente
+	 */
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
 
-	public void getNombre(String nombre) {
-		this.nombre = nombre;
+	/**
+	 * Devuelve el valor de la variable nombre
+	 * 
+	 * @return nombre del cliente
+	 */
+	public String getNombre() {
+		return nombre;
 	}
 
-	public void getApellidos(String apellidos) {
-		this.apellidos = apellidos;
+	/**
+	 * Devuelve el valor de la variable apellidos
+	 * 
+	 * @return apellidos del cliente
+	 */
+	public String getApellidos() {
+		return apellidos;
 	}
 
+	/**
+	 * Representa al cliente de la manera deseada
+	 * 
+	 * @return representacion del cliente
+	 */
+	@Override
 	public String toString() {
-		return "-------------------\nCliente:\nNombre:" + nombre + "\nApellido: " + apellidos
-				+ "\n-------------------\n";
+		return "-------------------\nCliente:\nNombre:" + nombre + "\nApellido: " + apellidos + "\n";
 	}
 }
