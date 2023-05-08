97db35a46d7dd664d4855753f2ce5a101f3f2eee
unknown <ppi0002@alu.medac.es>
2023-05-08 10:57:38 +0200

    transferencia

--- prBanco/src/prBanco/Operaciones.java
+++ prBanco/src/prBanco/Operaciones.java
@@ -2,4 +2,16 @@ package prBanco;
 
 public class Operaciones {
 
+	/**
+	 * Con este método cambiamos la cantidad que queremos de una cuenta a otra
+	 * 
+	 * @param c1 cuenta 1
+	 * @param c2 cuenta 2
+	 * @param cantidad cantidad de dinero a camciar de cuenta
+	 */
+	public void transferencia(Cuenta c1,Cuenta c2, double cantidad) {
+		c1.setSaldo(c1.getsaldo()-cantidad); 
+		c2.setSaldo(c2.getsaldo()+cantidad); 
+	}
+		
 }
